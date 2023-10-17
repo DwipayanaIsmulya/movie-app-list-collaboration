6564cdeaade005c4644ae825d6203ba244e46d9a
Ariantika Putri <arin.dila02@gmail.com>
2023-10-17 23:13:07 +0800

    add feature: login

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,19 +2,22 @@ import {
   Container,
   Navbar,
   Nav,
+  NavDropdown,
   Offcanvas,
   Form,
   InputGroup,
   Button,
 } from "react-bootstrap";
-import { BsSearch } from "react-icons/bs";
-import { useState } from "react";
+import { BsSearch, BsPersonCircle } from "react-icons/bs";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import styles from "./navbar.module.css";
 
 function Header() {
   const [navbar, setNavbar] = useState(false);
   const [query, setQuery] = useState("");
+  const [user, setUser] = useState(null);
 
   const navigate = useNavigate();
 
@@ -33,6 +36,54 @@ function Header() {
     navigate(`/search/?query=${query}`);
   };
 
+  const logout = (event) => {
+    event.preventDefault();
+
+    localStorage.removeItem("token");
+
+    // Redirect to home or reload the home
+    // This is temporary solution, the better solution is using redux
+    window.location.replace("/");
+  };
+
+  useEffect(() => {
+    const getMe = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) return;
+
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_AUTH_URL}/api/v1/auth/me`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const { data } = response.data;
+
+        // Set the user state from API data
+        setUser(data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          // If token is not valid
+          if (error.response.status === 401) {
+            localStorage.removeItem("token");
+            return;
+          }
+
+          alert(error?.response?.data?.message);
+          return;
+        }
+
+        alert(error?.message);
+      }
+    };
+
+    getMe();
+  }, []);
+
   return (
     <>
       <Navbar
@@ -42,7 +93,7 @@ function Header() {
       >
         <Container fluid>
           <Navbar.Brand href="/">
-            <h1 style={{ color: "red", fontWeight: "800" }}>MOVIELIST</h1>
+            <h1 style={{ color: "	#c1071e", fontWeight: "800" }}>MOVIELIST</h1>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand`} />
           <Navbar.Offcanvas
@@ -67,12 +118,12 @@ function Header() {
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-center flex-grow-1">
-                <Form onSubmit={handleSearch} style={{ width: "100%" }}>
+                <Form onSubmit={handleSearch} style={{ width: "50%" }}>
                   <InputGroup>
                     <Form.Control
                       type="text"
                       value={query}
-                      placeholder="What do you want to watch?"
+                      placeholder="Search any movies"
                       aria-label="search"
                       className="bg-transparent border-danger rounded-pill"
                       onChange={(event) => setQuery(event.target.value)}
@@ -91,20 +142,45 @@ function Header() {
               <br />
               <Nav className="justify-content-center pe-2">
                 <div className="d-flex">
-                  <Button
-                    variant="outline-danger"
-                    className="me-3 rounded-pill"
-                    style={{ width: "100%" }}
-                  >
-                    Login
-                  </Button>
-                  <Button
-                    variant="danger"
-                    className="rounded-pill"
-                    style={{ width: "100%" }}
-                  >
-                    Register
-                  </Button>
+                  {user ? (
+                    <>
+                      <NavDropdown
+                        title={
+                          user ? (
+                            <>
+                              <BsPersonCircle
+                                className="me-1"
+                                style={{ fontSize: "20px" }}
+                              />{" "}
+                              {user.name}
+                            </>
+                          ) : (
+                            <BsPersonCircle className="me-2" />
+                          )
+                        }
+                        menuVariant="dark"
+                        style={{ width: "100%" }}
+                      >
+                        <NavDropdown.Item as={Link} to="/myprofile">
+                          My Profile
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item as={Link} onClick={logout}>
+                          Logout
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                    </>
+                  ) : (
+                    <Button
+                      as={Link}
+                      to="/login"
+                      variant="outline-danger"
+                      className="me-3 rounded-pill"
+                      style={{ width: "100%" }}
+                    >
+                      Login
+                    </Button>
+                  )}
                 </div>
               </Nav>
             </Offcanvas.Body>
