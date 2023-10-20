bad53c0d853567ec5662c352cb0993a91f2393a5
Ariantika Putri <arin.dila02@gmail.com>
2023-10-20 17:45:56 +0800

    update: login & regis page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import Particle from "../Components/Particles/Particle";
-import { Form, Button, Container, Card } from "react-bootstrap";
+import { Form, Button, Container, Card, Row, Col } from "react-bootstrap";
 import styles from "../Components/Login/styles.module.css";
 import { Link } from "react-router-dom";
 
@@ -56,68 +56,88 @@ const Register = () => {
 
       <Container className="d-flex justify-content-center align-items-center vh-100">
         <Card className={styles["card"]}>
-          <Card.Body>
-            <Form onSubmit={onSubmit} className="mb-4">
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label className="d-flex justify-content-center">
-                  <h3 style={{ fontWeight: "800" }}>Sign Up</h3>
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  style={{ height: "45px" }}
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </Form.Group>
+          <Row className="d-flex justify-content-center align-items-center">
+            <Col>
+              <Card.Body>
+                <h1>
+                  Welcome to{" "}
+                  <span style={{ color: "	#c1071e", fontWeight: "800" }}>
+                    MOVIELIST
+                  </span>
+                  !
+                </h1>
+                <h5 className="my-3">
+                  Create your{" "}
+                  <span style={{ color: "	#c1071e" }}>new account</span> to
+                  Access more Movies.
+                </h5>
+              </Card.Body>
+            </Col>
+            <Col>
+              <Card.Body className="border border-2 rounded">
+                <Form onSubmit={onSubmit} className="mb-4">
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label className="d-flex justify-content-center">
+                      <h3 style={{ fontWeight: "800" }}>Sign Up</h3>
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Username"
+                      style={{ height: "45px" }}
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control
-                  type="email"
-                  placeholder="Email"
-                  style={{ height: "45px" }}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control
+                      type="email"
+                      placeholder="Email"
+                      style={{ height: "45px" }}
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  style={{ height: "45px" }}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
-              <Button
-                variant="danger"
-                type="submit"
-                style={{ width: "100%" }}
-                className="mb-3"
-              >
-                Sign Up
-              </Button>
-              <hr className="my-1" />
-              <div className="d-flex justify-content-center my-3 ">
-                <span style={{ fontSize: "15px", color: "GrayText" }}>
-                  Have Already Account?
-                </span>
-                <Link
-                  as={Link}
-                  to="/login"
-                  style={{
-                    textDecoration: "none",
-                    color: "#dc3545",
-                    fontSize: "14px",
-                    paddingLeft: "4px",
-                  }}
-                >
-                  Sign In now
-                </Link>
-              </div>
-            </Form>
-          </Card.Body>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      style={{ height: "45px" }}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button
+                    variant="danger"
+                    type="submit"
+                    style={{ width: "100%" }}
+                    className="mb-3"
+                  >
+                    Sign Up
+                  </Button>
+                  <hr className="my-1" />
+                  <div className="d-flex justify-content-center my-3 ">
+                    <span style={{ fontSize: "15px", color: "GrayText" }}>
+                      Have Already Account?
+                    </span>
+                    <Link
+                      as={Link}
+                      to="/login"
+                      style={{
+                        textDecoration: "none",
+                        color: "#dc3545",
+                        fontSize: "14px",
+                        paddingLeft: "4px",
+                      }}
+                    >
+                      Sign In now
+                    </Link>
+                  </div>
+                </Form>
+              </Card.Body>
+            </Col>
+          </Row>
         </Card>
       </Container>
     </>
